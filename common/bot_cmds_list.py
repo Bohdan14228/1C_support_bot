d291d238de43e80e9be3f36b8130a1d5c65ab2f1
Bohdan <onlyolx1488@gmail.com>
2024-02-20 23:58:13 +0200

    start video fsm

--- common/bot_cmds_list.py
+++ common/bot_cmds_list.py
@@ -1,7 +1,7 @@
 from aiogram.types import BotCommand
 
+
 private = [
-    BotCommand(command='start', description='Старт'),
     BotCommand(command='menu', description='Посмотреть меню'),
     BotCommand(command='about', description='О нас'),
     BotCommand(command='payment', description='Варианты оплаты'),
